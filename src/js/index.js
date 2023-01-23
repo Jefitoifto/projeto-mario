de2296467c276e32aa0634df9c85262e5c2c5cc5
Jefitoifto <jeferson.santos4@estudante.ifto.edu.br>
2023-01-23 14:44:19 -0300

    refatorando últimos detalhes

--- src/js/index.js
+++ src/js/index.js
@@ -1,26 +1,20 @@
-const video = document.getElementById("video");
 const botaoTrailer = document.querySelector(".botao-trailer");
+const botaoFechar = document.querySelector(".fechar-modal");
 const modal = document.querySelector(".modal");
+const video = document.getElementById("video");
 const linkVideo = video.src;
 
-//passo 1 - dar um jeito de pegar o elemento que representa o botão na tela usando js
-console.log('mostrar o document', document);
-console.log(document.querySelector(".botao-trailer"));
+function alterarModal(){
+    modal.classList.toggle("aberto");
+}
 
-//passo 2 - dar um jeito de identificar quando o usuário clicar no botão
 botaoTrailer.addEventListener("click", () => {
-    //passo 4 - abrir a modal na tela
-    modal.classList.add("aberto");
+    //abrir a modal na tela
+    alterarModal();
     video.setAttribute("src", linkVideo);
-
 });
-
 //OBJETIVO 2 - quando o usuário clicar no X devemos fechar a modal
-//passo 1 - dar um jeito de pegar o elemento de fechar a modal usando o js
-botaoFechar = document.querySelector(".fechar-modal");
-
-//passo 2 - dar um jeito de identificar quando o usuário clicar no botão X
 botaoFechar.addEventListener("click", () => {
-    modal.classList.remove("aberto");
+    alterarModal();
     video.setAttribute("src", "");
 });
